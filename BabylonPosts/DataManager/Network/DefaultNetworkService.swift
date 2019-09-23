5a7c8a302e34ea0ebc7e5ffd6255815a7a505c6c
Li Linyu <li.linyu@gmail.com>
2019-09-23 20:44:02 +0100

    Removed duplicated logic by using Generics.

--- BabylonPosts/DataManager/Network/DefaultNetworkService.swift
+++ BabylonPosts/DataManager/Network/DefaultNetworkService.swift
@@ -21,68 +21,36 @@ final class DefaultNetworkService: NetworkService {
 
     func fetchPosts() -> Promise<[Post]> {
         let resource = Resource.posts.resource
-        let url = "\(serviceUrl)\(resource.route)"
-
-        return Promise { seal in
-            AF.request(url,
-                       method: resource.method,
-                       headers: DefaultNetworkService.defaultHeader)
-                .responseDecodable(of: [Post].self) { response in
-                    if let error = response.error {
-                        seal.reject(error)
-                        return
-                    }
-
-                    if let posts = response.value {
-                        seal.fulfill(posts)
-                    } else {
-                        seal.reject(NetworkServiceError.nilResponseValueError())
-                    }
-            }
-        }
+        return fetchData(method: resource.method, route: resource.route)
     }
 
     func fetchUser(for userId: Int) -> Promise<User> {
         let resource = Resource.user(userId: userId).resource
-        let url = "\(serviceUrl)\(resource.route)"
-
-        return Promise { seal in
-            AF.request(url,
-                       method: resource.method,
-                       headers: DefaultNetworkService.defaultHeader)
-                .responseDecodable(of: [User].self) { response in
-                    if let error = response.error {
-                        seal.reject(error)
-                        return
-                    }
-
-                    if let user = response.value?.first {
-                        seal.fulfill(user)
-                    } else {
-                        seal.reject(NetworkServiceError.nilResponseValueError())
-                    }
-            }
-        }
+        return fetchData(method: resource.method, route: resource.route)
     }
 
     func fetchComments(for postId: Int) -> Promise<[Comment]> {
         let resource = Resource.comments(postId: postId).resource
-        let url = "\(serviceUrl)\(resource.route)"
+        return fetchData(method: resource.method, route: resource.route)
+    }
+
+    private func fetchData<T: Codable>(method: HTTPMethod, route: String) -> Promise<T> {
+        let url = "\(serviceUrl)\(route)"
 
         return Promise { seal in
             AF.request(url,
-                       method: resource.method,
+                       method: method,
                        headers: DefaultNetworkService.defaultHeader)
-                .responseDecodable(of: [Comment].self) { response in
+                .responseDecodable(of: T.self) { response in
                     if let error = response.error {
-                        seal.reject(error)
+                        seal.resolve(nil, error)
                         return
                     }
 
-                    if let comments = response.value {
-                        seal.fulfill(comments)
+                    if let data = response.value {
+                        seal.resolve(data, nil)
                     } else {
-                        seal.reject(NetworkServiceError.nilResponseValueError())
+                        seal.resolve(nil, NetworkServiceError.nilResponseValueError())
                     }
             }
         }
